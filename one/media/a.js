0e94c14f3e71ce2c49b5a5276217d6cbf9ab03f2
we <helloxingbang@gmail.com>
2021-10-11 23:19:30 +0800

    2

--- one/media/a.js
+++ one/media/a.js
@@ -2,6 +2,6 @@
     let stream = await navigator.mediaDevices.getUserMedia({video: false, audio: true})
 
     let videos = await navigator.mediaDevices.getDisplayMedia({video: true, audio: false})
-    document.querySelector("video").srcObject = videos;
+    document.getElementById("video").srcObject = videos;
     document.querySelector("audio").srcObject = stream;
 })();
